9c9d85b915b3349a28c6e6efa9c12590fc423e69
Artem Scherbakov <scherbakov@softlab.ru>
2026-05-06 20:19:44 +0300

    add sonar

---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("java")
+    id("jacoco")
+    id("org.sonarqube") version "7.3.0.8198"
+    checkstyle
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "artem90s_java-project-78")
+        property("sonar.organization", "artem90s")
+        property ("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+
+    }
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
